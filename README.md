c3d89eceac8d2cc7dc559e42be19aecbaf172d86
EduardE02 <eduard@rantsevich.net>
2020-11-24 20:51:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Human Benchmark  
+This is a bot that klicks the screen as fast as possible when it turns green  
+You only have to modify the pixel and the mouse position according to your monitor and window location  
---
+++ reaction.py
@@ -0,0 +1,39 @@
+import mss
+import numpy as np
+import time
+from pynput.mouse import Button, Controller
+
+pixel_x, pixel_y = 2000, 500
+
+
+# noinspection PyTypeChecker
+def main():
+    mouse = Controller()
+    sct = mss.mss()
+    monitor = {'top': pixel_y, 'left': pixel_x, 'width': 1, 'height': 1}
+    counter = 0
+    t = time.time()
+    n = 1
+    c2 = 0
+    mouse.position = (pixel_x, pixel_y)
+    while c2 < 5:
+        counter += 1
+        img = np.asarray(sct.grab(monitor))
+        if time.time() - t >= n:
+            t = time.time()
+            print("FPS:", counter)
+            counter = 0
+        if img[0][0][1] > 200:
+            mouse.press(Button.left)
+            mouse.release(Button.left)
+            c2 += 1
+            time.sleep(0.5)
+            print("clicked")
+        elif img[0][0][0] > 150:
+            mouse.press(Button.left)
+            mouse.release(Button.left)
+            time.sleep(0.5)
+
+
+if __name__ == "__main__":
+    main()
